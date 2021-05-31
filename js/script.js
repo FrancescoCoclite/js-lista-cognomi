0667f458a2e54a2fe02912fe4aeb019547bf5161
Francesco <francescococlite@gmail.com>
2021-05-31 15:03:36 +0200

    ho fatto un danno con le cartelle

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@
 var email = prompt('Inserisci la tua Email');
 console.log(email);
 
-var listaMail = ['exemple@email.com', 'exemple@email.it', 'exemple@email.org', 'exemple@email.ul'];
-console.log(listaMail); 
+var listaMail = ['exemple@email.com', 'exemple@email.it', 'exemple@email.org', 'exemple@email.ul']; 
 
 var user = 'Utente non autorizzato';
 
